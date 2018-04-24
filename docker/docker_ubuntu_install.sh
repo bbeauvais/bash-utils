466c17a38d0457a45ea5c37bac01cb2a40bb4129
Baptiste Beauvais <bbeauvais@groupefbo.com>
2018-04-24 13:57:45 +0200

    Utilisation de " pour délimiter les strings

--- docker/docker_ubuntu_install.sh
+++ docker/docker_ubuntu_install.sh
@@ -2,14 +2,14 @@
 # Script pour installter le daemon Docker CE et Docker compose en production sur un système Ubuntu
 # Le script suppose que l'utilisateur à les droits suffisant pour faire l'installation
 
-echo Mise de l\'index de aptitude
+echo "Mise de l\'index de aptitude"
 apt update
 
 
-echo Installation des packages secondaire necessaire
+echo "Installation des packages secondaire necessaire"
 apt install apt-transport-https ca-certificates curl software-properties-common
 
-echo Ajout de la cle officiel GPG
+echo "Ajout de la cle officiel GPG"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
 apt-key fingerprint Docker
@@ -17,7 +17,7 @@ apt-key fingerprint Docker
 while read -n1 -r -p "\nVerifier que la cle est bien celle qui est attendue (y=continuer, n=arreter) : " && [[ $REPLY != y ]]; do
 	case $REPLY in
 		n) exit 0;;
-		*) echo "Reponse incorrecte y=continuer, n=arreter : "
+		*) echo "Reponse incorrecte y=continuer, n=arreter : ";;
 	esac
 done
 
