58964e685b04b0f3736c8d36600eafc0b61c3249
baptiste <beauvbaptiste@gmail.com>
2018-07-25 07:20:20 +0000

    Choix de la version de docker-compose

--- docker/docker_compose_install.sh
+++ docker/docker_compose_install.sh
@@ -1,10 +1,16 @@
 # !/bin/bash
 # Installation de Docker compose sur une machine UNIX
 
-echo "Installation de Docker compose"
-curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-curl -L https://raw.githubusercontent.com/docker/compose/1.21.0/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+echo installation de docker compose
 
-chmod +rx /usr/local/bin/docker-compose
+echo Quelle version utiliser ? https://github.com/docker/compose/releases
+
+read version
+
+echo "Installation de Docker compose"
+curl -L https://github.com/docker/compose/releases/download/$version/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+curl -L https://raw.githubusercontent.com/docker/compose/$version/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+
+chmod +x /usr/local/bin/docker-compose
 
 docker-compose --version
